ba3fbbf7efcce961ac621623bf0450fd29c22092
StarDust130 <csyadav0513@gmail.com>
2026-05-16 14:05:38 +0530

    add: create environment configuration schema using zod

---
+++ apps/core/src/config/env.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  PORT: z.string().default("8080"),
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string(),
+  QDRANT_URL: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
